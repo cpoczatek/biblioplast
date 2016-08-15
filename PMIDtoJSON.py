1e2d819e8924bebaa72d8cca2ca877364a9edf7b
Collin <collin.poczatek@gmail.com>
2016-08-15 15:58:28 -0400

    mostly working

---
+++ PMIDtoJSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# Reads a list of PMID's (one per line) and outputs full info as JSON.
+# May need to run: pip install requests xmltodict json
+# Usage: echo "12345678" | PMIDtoJSON.py or
+# cat ListOfPMIDs.txt | PMIDtoJSON.py > json.txt
+
+import fileinput
+import requests
+import xmltodict
+import json
+
+for line in fileinput.input():
+    id = line.strip()
+    r = requests.get('http://www.ncbi.nlm.nih.gov/pubmed/' + id + '?report=xml')
+    doc = xmltodict.parse(r.text.replace('&lt;','<').replace('&gt;','>'))['pre']
+    print(json.dumps(doc['PubmedArticle']['MedlineCitation'], indent=4))
+    print "\n\n##############################################################\n\n"
